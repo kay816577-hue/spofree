2c9885966ce4804b1b42e834b6ae61936a1a3447
kay816577-hue <kay816577@gmail.com>
2026-05-16 11:20:01 +0200

    Update API instances: Set Monochrome as default, add Qobuz and Dabmusic.xyz

--- constants.ts
+++ constants.ts
@@ -2,21 +2,23 @@
 import { Track } from './types';
 
 export const API_INSTANCES = [
+  'https://arran.monochrome.tf',
+  'https://monochrome.tf',
+  'https://qobuz.hifi-one.spotisaver.net',
+  'https://dabmusic.xyz',
   'https://vogel.qqdl.site',
   'https://katze.qqdl.site',
   'https://hund.qqdl.site',
-  'https://arran.monochrome.tf',
   'https://triton.squid.wtf',
   'https://wolf.qqdl.site',
   'https://maus.qqdl.site',
   'https://tidal.kinoplus.online',
   'https://tidal-api.binimum.org',
-  'https://triton.squid.wtf',
   'https://hifi-one.spotisaver.net',
   'https://hifi-two.spotisaver.net',
 ];
 
-// Select the first instance by default
+// Select the first instance (Monochrome API) by default
 export const API_BASE_URL = API_INSTANCES[0];
 
 export const DEFAULT_VOLUME = 0.5;
